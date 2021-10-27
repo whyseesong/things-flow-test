3fbbe65c2d5ef9c525d9ab8b13ca6022462edf3b
SongYoungCheol <yc@sk.com>
2021-10-27 20:53:03 +0900

    헤더 구현

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  const Title = styled.h1`
+    font-size: 30px;
+  `;
+  return <Title>Angular / Angular-cli</Title>;
+};
+
+export default Header;
