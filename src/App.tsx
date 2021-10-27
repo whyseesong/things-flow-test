e2b10c72bc5c59f3fc0ef3fa202552e9052de8d9
SongYoungCheol <yc@sk.com>
2021-10-27 20:50:22 +0900

    라우팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import React from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 
 import { Octokit } from "octokit";
+import styled from "styled-components";
+import Header from "./components/Header/Header";
 import { OctokitContext } from "./contexts/OctokitContext";
 
 function App() {
-  const auth = "***";
+  const AppWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `;
+
+  const auth = "**";
 
   return (
     <OctokitContext.Provider value={new Octokit({ auth })}>
-      {123}
+      <BrowserRouter>
+        <AppWrapper>
+          <Link to="/">
+            <Header />
+          </Link>
+          <Switch>
+            <Route exact path={"/"}>
+                <Link to='/1'>1</Link>
+                <Link to='/2'>2</Link>
+                <Link to='/3'>3</Link>
+            </Route>
+            <Route path={"/:no"} children={<div>page</div>} />
+          </Switch>
+        </AppWrapper>
+      </BrowserRouter>
     </OctokitContext.Provider>
   );
 }
